30f2269ae446c62c66c51a12673f9dae48093d51
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:48:57 -0700

    Begginer

--- URI(1074)Even or Odd.c
+++ URI(1074)Even or Odd.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int N,i,num;
+
+    scanf("%d",&N);
+    for(i = 1; i <= N; i++){
+       scanf("%d",&num);
+       if(num == 0){
+            printf("NULL\n");
+       }
+       else if(num > 0 && num%2 != 0){
+                printf("ODD POSITIVE\n");
+       }
+       else if(num < 0 && num%2 != 0){
+                printf("ODD NEGATIVE\n");
+       }
+       else if(num > 0 && num%2 != 1){
+                printf("EVEN POSITIVE\n");
+       }
+       else if(num < 0 && num%2 != 1){
+               printf("EVEN NEGATIVE\n");
+       }
+    }
+    return 0;
+}
 
