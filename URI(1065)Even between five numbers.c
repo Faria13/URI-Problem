17d55e68ad78cbda8b6305e6ebeb63706e45617a
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:46:51 -0700

    Even

--- URI(1065)Even between five numbers.c
+++ URI(1065)Even between five numbers.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i,num,even_num = 0;
+
+    for(i = 1; i <= 5; i++){
+        scanf("%d",&num);
+        if(num%2 == 0)
+            even_num++;
+    }
+    printf("%d valores pares\n",even_num++);
+
+    return 0;
+}
+
 
