849bbe9e2c2daa469ba8c4eac04253b97ea3522c
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:44:21 -0700

    Begginer

--- URI(1043)Triangle.c
+++ URI(1043)Triangle.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    float A,B,C,Area,Perimetro;
+    scanf("%f %f %f",&A,&B,&C);
+    
+    if(A+B>C && B+C>A && C+A>B){
+            
+        Perimetro = A+B+C;
+        printf("Perimetro = %0.1f\n",Perimetro);
+    }
+    else {
+        Area = .5*(A+B)*C;
+        printf("Area = %0.1f\n",Area);
+    }
+
+    return 0;
+}
+
 
