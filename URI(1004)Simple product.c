161478536feee0e9bb94b846b92e26e4603b1a5d
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:36:10 -0700

    Update URI(1004)Simple product.c
    Multiplication

--- URI(1004)Simple product.c
+++ URI(1004)Simple product.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,mul,PROD;
+
+    scanf("%d %d",&a,&b);
+
+    mul = a * b;
+
+    PROD = mul;
+
+    printf("PROD = %d\n",PROD);
+
+    return 0;
+}
+
 
