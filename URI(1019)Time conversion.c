4ba94fad7965dd7295413889e098723c22fe5f04
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:41:51 -0700

    Time change

--- URI(1019)Time conversion.c
+++ URI(1019)Time conversion.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int N,minutes,hours,seconds,a;
+    scanf("%d",&N);
+    hours = N/3600;
+    a = N%3600;
+    minutes = a/60;
+    seconds = a%60;
+    printf("%d:%d:%d\n",hours,minutes,seconds);
+    return 0;
+}
+
 
