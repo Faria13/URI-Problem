f44e563b9acd5247715579e70f40921b0e810861
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:55:52 -0700

    Multiple

--- URI(1132)Multiples of 13.c
+++ URI(1132)Multiples of 13.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int X,Y,i,sum,temp;
+    
+    sum = 0;
+    scanf("%d %d",&X,&Y);
+    if(X>Y){
+        temp = X;
+        X = Y;
+        Y = temp;
+
+    for(i = X; i <= Y; i++){
+        if(i%13 != 0)
+        sum += i;
+    }
+    printf("%d\n",sum);
+  }
+   else{
+    for(i = X; i <= Y; i++){
+        if(i%13 != 0)
+        sum += i;
+    }
+    printf("%d\n",sum);
+  }
+    return 0;
+}
+
 
