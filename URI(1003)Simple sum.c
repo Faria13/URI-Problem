6d51caed649d3320afb3eefa4498f87f6f409cd7
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:34:38 -0700

    Basic
    Sum

--- URI(1003)Simple sum.c
+++ URI(1003)Simple sum.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int A,B,sum,SOMA;
+
+    scanf("%d %d",&A,&B);
+
+    sum = A + B;
+
+    SOMA = sum;
+
+    printf("SOMA = %d\n",SOMA);
+
+    return 0;
+}
+
 
