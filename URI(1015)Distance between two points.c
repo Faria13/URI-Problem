d6911e84e76a1c57438787c3f4bc4b5595353c2e
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:40:38 -0700

    Distance

--- URI(1015)Distance between two points.c
+++ URI(1015)Distance between two points.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main()
+{
+    double x1,x2,y1,y2,p1,p2,sum,distance;
+
+    scanf("%lf %lf\n",&x1,&y1);
+
+    scanf("%lf %lf",&x2,&y2);
+
+    distance = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+
+    printf("%0.4f\n",distance);
+
+    return 0;
+}
+
 
