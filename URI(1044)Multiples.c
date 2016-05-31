ad1b5a110eb627937187a3c4a8c6d1ec34b13475
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:45:35 -0700

    Multiple

--- URI(1044)Multiples.c
+++ URI(1044)Multiples.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int A,B,temp;
+    scanf("%d%d",&A,&B);
+
+    if(A>B){
+        temp = A;
+        A = B;
+        B = temp;
+
+        if(B>A && B%A == 0)
+            printf("Sao Multiplos\n");
+        else
+            printf("Nao sao Multiplos\n");
+    }
+    else{
+        if(B>A && B%A == 0)
+            printf("Sao Multiplos\n");
+        else
+            printf("Nao sao Multiplos\n");
+    }
+
+    return 0;
+}
+
 
