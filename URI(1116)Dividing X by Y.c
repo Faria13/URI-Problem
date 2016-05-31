47b4fd46c69ded1705dcbd5b4cede1cba5e7d052
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:54:37 -0700

    Division

--- URI(1116)Dividing X by Y.c
+++ URI(1116)Dividing X by Y.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int X,Y,T,i;
+    double a;
+
+    scanf("%d",&T);
+
+    for(i = 1; i <= T; i++){
+        scanf("%d %d",&X,&Y);
+        a = (float) X/Y;
+        if(Y == 0){
+            printf("divisao impossivel\n");
+        }
+        else{
+            printf("%0.1lf\n",a);
+        }
+    }
+    return 0;
+}
+
 
