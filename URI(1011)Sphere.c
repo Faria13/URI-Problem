f82cb45971a05962fad591747f67e0baaa0f52b7
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:38:40 -0700

    Sphere

--- URI(1011)Sphere.c
+++ URI(1011)Sphere.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main()
+{
+    int R;
+
+    double VOLUME;
+
+    scanf("%d",&R);
+
+    VOLUME = (4/3.0)* PI *R*R*R;
+
+    printf("VOLUME = %0.3lf\n",VOLUME);
+
+    return 0;
+}
+
 
