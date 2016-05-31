2d048e54c90bbd848b0f61715a83f62e83107a21
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:49:55 -0700

    Average

--- URI(1079)Weighted Averages.c
+++ URI(1079)Weighted Averages.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int T,i;
+    float a,b,c,average;
+    scanf("%d",&T);
+
+    for(i = 1; i <= T; i++){
+        scanf("%f %f %f",&a,&b,&c);
+
+        average = (a*2+b*3+c*5)/10.0;
+
+        printf("%0.1f\n",average);
+    }
+    return 0;
+
+}
+
 
