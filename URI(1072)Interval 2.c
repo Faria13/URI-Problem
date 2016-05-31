0ef1aa1abba5967189d6525db67c29a089d674ba
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:48:01 -0700

    Easy

--- URI(1072)Interval 2.c
+++ URI(1072)Interval 2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int N,i,in = 0,out = 0;
+
+    int X;
+
+    scanf("%d",&N);
+    for(i = 1; i <= N; i++){
+        scanf("%d",&X);
+        if(X >= 10 && X <= 20)
+            in++;
+        else
+            out++;
+    }
+    printf("%d in\n%d out\n",in++,out++);
+
+    return 0;
+}
 
