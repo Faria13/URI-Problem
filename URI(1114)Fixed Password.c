14cb8e3607c353340047799dd1d2c710e6cbdd2d
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:53:17 -0700

    Password

--- URI(1114)Fixed Password.c
+++ URI(1114)Fixed Password.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    while(scanf("%d",&num)==1){
+        if(num == 2002)
+            printf("Acesso Permitido\n");
+        else
+            printf("Senha Invalida\n");
+        if(num == 2002)
+            break;
+    }
+    return 0;
+}
+
 
