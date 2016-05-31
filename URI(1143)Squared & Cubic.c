e65899e2ef864b2dc684fb538cf7307620e19813
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:57:08 -0700

    Easy

--- URI(1143)Squared & Cubic.c
+++ URI(1143)Squared & Cubic.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int N,i,count;
+    scanf("%d",&N);
+    for(i = 1; i <= N; i++){
+        if(i == 3)
+        count++;
+        printf("%d %d %d",i,i*i,i*i*i);
+        printf("\n");
+    }
+    return 0;
+}
+
 
