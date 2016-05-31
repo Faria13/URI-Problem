f164c8a511348a0a0d07c6ee719544aa06a067c1
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:58:11 -0700

    Simple math

--- URI(1153)Simple factorial.c
+++ URI(1153)Simple factorial.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int N,i,factorial = 1;
+
+    scanf("%d",&N);
+
+    for(i = N; i >= 1; i--){
+        factorial = factorial*i;
+    }
+    printf("%d\n",factorial);
+
+    return 0;
+}
+
 
