62d3568bdb6a94e72515879c1f183dcb826c4c7c
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-05-31 14:37:28 -0700

    Find Distance

--- URI(1007)Difference.c
+++ URI(1007)Difference.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int A,B,C,D,DIFERENCA,difference;
+
+    scanf("%d %d %d %d",&A,&B,&C,&D);
+
+    difference =( A * B - C * D );
+
+    DIFERENCA = difference;
+
+    printf("DIFERENCA = %d\n",DIFERENCA);
+
+    return 0;
+}
+
 
